ba64586075769525ded361cea99349e2585e1809
norbe98 <szoke.norbi18@gmail.com>
2026-03-11 13:21:53 +0100

    finding author

--- task1/task.js
+++ task1/task.js
@@ -1,5 +1,6 @@
 export function getLongestBookByAuthorName(authors, books, authorName) {
 
+    //FINDING AUTHOR
   const author = authors.find(author => author.name === authorName)
 
   if (!author) {
