69ce0917f494d8bd71d80ae5850c61f7bf7afc68
norbe98 <szoke.norbi18@gmail.com>
2026-03-11 13:33:15 +0100

    task1 finished

--- task1/task.js
+++ task1/task.js
@@ -1,10 +1,22 @@
 export function getLongestBookByAuthorName(authors, books, authorName) {
 
-    //FINDING AUTHOR
+    //Find the author
   const author = authors.find(author => author.name === authorName)
 
   if (!author) {
     return null
   }
+
+  //Author books
+  const allBooksByAuthor = books.filter(book => book.author_id === author.id)
+
+  allBooksByAuthor.sort((a, b) => {
+    if (b.pages !== a.pages) {
+      return b.pages - a.pages
+    }
+    return a.title.localeCompare(b.title)
+  })
+
+  return allBooksByAuthor[0].title
     
 }
